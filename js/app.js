1eb9a09bbb64f20487e7c87affcd76b3659747cc
brydenfogelman <bryden1995@gmail.com>
2015-04-22 11:12:15 -0700

    New Feature
    Recap of work completed
    - updated comments
    - add a new function that would grab the ranked stats

--- js/app.js
+++ js/app.js
@@ -9,20 +9,21 @@ $(document).on('ready', function(){
 	// Globals
 	var API_KEY = "?api_key=7b52d012-93d1-43b9-b84d-c8420a3824f9";
 	var base_url = "https://na.api.pvp.net/api/lol/";
-	var region = $("#region-field option:selected").val();
 	// setting the base_url with the region
-	base_url = base_url + region
+	//base_url = base_url + region
 
 	$('#btn-submit').on('click', function(e){
 		e.preventDefault();
 		
 		var url;
 		var summoner = $("#summoner-field").val().replace(/\s+/g, '');
+		var region = $("#region-field option:selected").val();
+		console.log(region)
 		//console.log(summoner);
 		
 		// base_url + region + '/v1.4/summoner/by-name/' + summoner + API_KEY;
 		 // region instead of base url
-		url = base_url + '/v1.4/summoner/by-name/' + summoner + API_KEY;
+		url = base_url + region + '/v1.4/summoner/by-name/' + summoner + API_KEY;
 		//console.log(url);
 		get_sum_id(url, summoner, get_stat);
 	});
@@ -62,6 +63,7 @@ $(document).on('ready', function(){
 			url: url,
 			success: function(data){
 				console.log(data);
+				manage_summoner_stats(data);
 			},
 			error: function(error){
 				console.log(error);
@@ -69,7 +71,22 @@ $(document).on('ready', function(){
 		});
 	};
 
-
+	/**
+	* Handles the json response from the summoner api stat request.
+	* @param json object
+	*/
+	function manage_summoner_stats(json_response){
+		// console.log(json_response["playerStatSummaries"]);
+		//RankedSolo5x5
+		var array = json_response["playerStatSummaries"];
+		for(var index = 0; index < array.length; index++){
+			var type = array[index].playerStatSummaryType;
+			if(type === "RankedSolo5x5"){
+				//aggregatedStats
+				console.log(array[index])
+			}
+		}
+	};
 	// grab the id from the input in focus
 	// can use in future implementations with .keyup
 	// var id;
